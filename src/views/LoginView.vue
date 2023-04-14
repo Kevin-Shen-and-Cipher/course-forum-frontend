d94b2c3662d51bf67ab5ae02929742e7e63e9e80
ian901204 <scckate444@gmail.com>
2023-04-14 19:45:07 +0800

    feat: add login.vue and loginView.vue file

---
+++ src/views/LoginView.vue
@@ -0,0 +1,17 @@
+<template>
+        <v-row align="center" justify="center">
+            <login>
+            </login>
+        </v-row>
+</template>
+
+<script>
+import Login from "@/components/Login.vue";
+
+export default {
+    components: {Login},
+};
+</script>
+
+<style>
+</style>
