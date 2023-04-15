ed1ee84b9c56626b981d60ef0a20fad64d936c01
ian901204 <scckate444@gmail.com>
2023-04-15 15:32:20 +0800

    feat: add post and search componments

---
+++ src/components/Home/Search.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="d-flex justify-center align-center" style="width: 400px;height: 90px; padding-top: 30px">
+        <v-text-field
+                :loading="loading"
+                density="compact"
+                variant="solo"
+                label="搜尋"
+                append-inner-icon="mdi-magnify"
+                single-line
+                hide-details
+                @click:append-inner="onClick"
+        ></v-text-field>
+    </div>
+</template>
+<script>
+export default {
+    data: () => ({
+        loaded: false,
+        loading: false,
+    }),
+    methods: {
+        onClick() {
+            this.loading = true
+
+            setTimeout(() => {
+                this.loading = false
+                this.loaded = true
+            }, 2000)
+        },
+    },
+}
+</script>
+<style>
+.search {
+    width: 400px;
+}
+</style>
