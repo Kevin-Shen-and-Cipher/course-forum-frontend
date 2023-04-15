47cd2c4751972142512e8d282a9467d26660cd0f
ian901204 <scckate444@gmail.com>
2023-04-15 15:34:37 +0800

    feat: change template flex

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
     <v-app>
+<<<<<<< HEAD
         <div class="d-flex flex-column" style="height: 100%">
+=======
+        <div class="d-flex flex-column" style = "height: 100%">
+>>>>>>> f10a358 (feat: change template flex)
             <div class="d-flex flex-column" style="height: 64px">
                 <top-bar/>
             </div>
@@ -13,8 +17,22 @@
         </div>
     </v-app>
 </template>
+<<<<<<< HEAD
 <script setup>
     import {RouterView} from 'vue-router';
     import Footer from '@/components/Footer.vue';
     import TopBar from "@/components/TopBar.vue";
+=======
+<script>
+import {RouterView} from 'vue-router';
+import Footer from '@/components/Footer.vue';
+import TopBar from "./components/TopBar.vue";
+
+export default {
+    components: {
+        Footer,
+        TopBar
+    }
+}
+>>>>>>> f10a358 (feat: change template flex)
 </script>
