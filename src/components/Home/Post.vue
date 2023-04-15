d52c92bc30e4b9420a4f0bf079098a9f91530905
ian901204 <scckate444@gmail.com>
2023-04-15 15:32:20 +0800

    feat: add post and search componments

---
+++ src/components/Home/Post.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="post d-flex">
+    </div>
+</template>
+<script>
+export default {}
+</script>
+<style>
+.post {
+    background-color: rgba(232, 230, 230, 1);
+    height: 150px;
+    width: 650px;
+    margin: 20px;
+    border-radius: 10px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,66 @@
 <template>
-</template>
+    <div class="d-flex justify-center overflow-auto" style="width: 100%">
+        <div class="d-flex flex-column align-center">
+            <Search/>
+            <div class="scroll-container" ref="scrollContainer">
+                <div class="scroll-content">
+                    <Post/>
+                    <Post/>
+                    <Post/>
+                    <Post/>
+                    <Post/>
+                    <Post/>
+                    <Post/>
+                    <Post/>
+                    <Post/>
+                    <Post/>
+                </div>
+            </div>
 
-<style scoped>
-</style>
-<script setup>
+        </div>
+    </div>
+
+</template>
+<script>
+import Post from "@/components/Home/Post.vue";
+import Search from "@/components/Home/Search.vue";
+
+export default {
+    components: {
+        Post,
+        Search
+    },
+    mounted() {
+        this.$nextTick(() => {
+            this.hideScrollbar();
+        });
+    },
+    methods: {
+        hideScrollbar() {
+            // 获取滚动容器 DOM 元素
+            const scrollContainer = this.$refs.scrollContainer;
+
+            // 当滚动容器内的内容不超出容器高度时，隐藏滚动条
+            if (scrollContainer.scrollHeight <= scrollContainer.clientHeight) {
+                scrollContainer.style.overflowY = 'hidden';
+            }
+        },
+    },
+
+}
 </script>
+<style>
+.scroll-container {
+    overflow-y: scroll;
+}
+
+.scroll-content {
+    /* Set width and height to match the content */
+    height: calc(75vh);
+}
+
+/* 隐藏滚动条的轨道和滑块 */
+::-webkit-scrollbar {
+    display: none;
+}
+</style>
