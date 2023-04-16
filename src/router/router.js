3c2605b617e31cf57cf1d9f534537a8c06a0c285
ian901204 <scckate444@gmail.com>
2023-04-16 19:31:01 +0800

    refactor: change home route component

--- src/router/router.js
+++ src/router/router.js
@@ -1,10 +1,12 @@
 import {createRouter, createWebHistory} from 'vue-router';
+import Home from '../views/Home.vue';
+import Login from '../views/LoginView.vue';
 
 const routes = [
     {
         path: '/',
         name: 'Home',
-        component: () => import("@/views/Home.vue")
+        component: () => import("@/views/HomeView.vue")
     },
     {
         path: '/login',
