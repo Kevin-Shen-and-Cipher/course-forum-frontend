bbce172416d5cb262637206634ad09a4dffee8d4
ian901204 <scckate444@gmail.com>
2023-04-16 20:10:57 +0800

    refactor: change to scroll bar css

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,30 +2,35 @@
     <div class="d-flex justify-center" style="width: 100%;">
         <div class="d-flex flex-column align-center">
             <Search/>
-            <div class="scroll-container" ref="scrollContainer" @scroll="handleScroll">
+            <div class="scroll-container light">
                 <Post v-for="i in 50"/>
             </div>
         </div>
     </div>
 </template>
+
 <script setup>
 import Post from "@/components/Home/Post.vue";
 import Search from "@/components/Home/Search.vue";
-import {onMounted, ref} from "vue";
-
-const scrollContainer = ref(null);
-
-function handleScroll() {
-    console.log("true");
-}
-
-onMounted(() => {
-    scrollContainer.value.style.overflowY = "scroll";
-});
 </script>
+
 <style>
 .scroll-container {
     overflow-y: scroll;
     height: 80vh;
+}
+
+.light::-webkit-scrollbar {
+    width: 15px;
+}
+
+.light::-webkit-scrollbar-thumb {
+    background: #b0b0b0;
+    border: solid 3px #e6e6e6;
+    border-radius: 20px;
+}
+
+.light::-webkit-scrollbar-thumb:hover {
+    background: black;
 }
 </style>
