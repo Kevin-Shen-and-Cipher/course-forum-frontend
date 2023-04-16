4743be568041b7cd0ba40a33b03efc3196b9ad0d
ian901204 <scckate444@gmail.com>
2023-04-16 19:28:00 +0800

    feat: change template format and add hidden scroll bar

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
             <div class="d-flex flex-column" style="height: 64px">
                 <top-bar/>
             </div>
-            <div class="d-flex" style="height: 100%;margin: 20px">
+            <div class="d-flex main-section">
                 <router-view/>
             </div>
             <div class="d-flex" style="height: 64px">
@@ -18,3 +18,13 @@ import {RouterView} from 'vue-router';
 import Footer from '@/components/Footer.vue';
 import TopBar from "@/components/TopBar.vue";
 </script>
+<style>
+body::-webkit-scrollbar {
+    display: none;
+}
+
+.main-section {
+    height: calc(90vh - 100px);
+    margin: 20px
+}
+</style>
