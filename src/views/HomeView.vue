1f17289caf82a27d42dc8e40050fcfeb6f11c4c8
ian901204 <scckate444@gmail.com>
2023-04-16 19:29:28 +0800

    feat: change Home.vue to HomeView.vue and add content

---
+++ src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="d-flex justify-center" style="width: 100%;">
+        <div class="d-flex flex-column align-center">
+            <Search/>
+            <div class="scroll-container" ref="scrollContainer" @scroll="handleScroll">
+                <Post v-for="i in 50"/>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+import Post from "@/components/Home/Post.vue";
+import Search from "@/components/Home/Search.vue";
+import {onMounted, ref} from "vue";
+
+const scrollContainer = ref(null);
+
+function handleScroll() {
+    console.log("true");
+}
+
+onMounted(() => {
+    scrollContainer.value.style.overflowY = "scroll";
+});
+</script>
+<style>
+.scroll-container {
+    overflow-y: scroll;
+    height: 80vh;
+}
+</style>
