5206af128823c4e910dfcf8ffaab0727368e6baa
ian901204 <scckate444@gmail.com>
2023-04-20 14:01:17 +0800

    refactor: remove merge error

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
     <v-app>
-<<<<<<< HEAD
         <div class="d-flex flex-column" style="height: 100%">
-=======
-        <div class="d-flex flex-column" style = "height: 100%">
->>>>>>> f10a358 (feat: change template flex)
             <div class="d-flex flex-column" style="height: 64px">
                 <top-bar/>
             </div>
@@ -17,25 +13,15 @@
         </div>
     </v-app>
 </template>
-<<<<<<< HEAD
 <script setup>
-<<<<<<< HEAD
 import {RouterView} from 'vue-router';
 import Footer from '@/components/Footer.vue';
 import TopBar from "@/components/TopBar.vue";
-<<<<<<< HEAD
-
-=======
-    import {RouterView} from 'vue-router';
-    import Footer from '@/components/Footer.vue';
-    import TopBar from "@/components/TopBar.vue";
-=======
 import {onMounted} from "vue";
 
 onMounted(() => {
     document.title = import.meta.env.VITE_APP_TITLE;
 });
->>>>>>> b96c342 (feat: change title in app.vue)
 </script>
 <style scoped>
 .main-section {
