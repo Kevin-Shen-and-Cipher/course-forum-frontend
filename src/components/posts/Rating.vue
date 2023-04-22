4dbe8ec420d6264cfc5a551d70bcf43cbaf37fce
ian901204 <scckate444@gmail.com>
2023-04-23 05:06:04 +0800

    feat:make view content to component

--- src/components/posts/Rating.vue
+++ src/components/posts/Rating.vue
@@ -20,19 +20,3 @@ const props = defineProps({
 });
 const rating = ref(props.rating);
 </script>
-
-const props = defineProps({
-    showLabel: {
-        type: Boolean,
-        default: false
-    },
-    ratingReadonly: {
-        type: Boolean,
-        default: false
-    },
-    rating:{
-        type: Number
-    }
-});
-const rating = ref(props.rating);
-</script>
