8b348c880d3d466bc81541659e316ae3db5aaea1
ian901204 <scckate444@gmail.com>
2023-04-24 17:50:57 +0800

    refactor: make props less

--- src/components/Home/Post.vue
+++ src/components/Home/Post.vue
@@ -1,82 +1,67 @@
 <template>
-    <div class="post d-flex" @click="inPost">
-        <div class="d-flex align-center justify-center" style="width: 15%">
-            <v-icon icon="mdi-forum" size="40"></v-icon>
-        </div>
-        <div class="d-flex" style="width: 60%">
-            <v-card color="transparent" border="0" variant="tonal">
-                <div class="v-card__text">
-                    <v-card-title>
-                        {{ title }}
-                    </v-card-title>
-                    <v-card-subtitle>
-                        {{ update_at.substring(0, 10) }}
-                    </v-card-subtitle>
-                    <v-card-text>
-                        {{ content }}
-                    </v-card-text>
-                    <a v-for="i in tag">
-                        {{ i.name }}
-                    </a>
-                </div>
-            </v-card>
-        </div>
-        <div class="d-flex align-center justify-end" style="width: 25%">
-            <div class="d-flex flex-column align-center justify-center" style="width: 60%">
-                <a>{{ views }}</a>
-                <a>Views</a>
-            </div>
-        </div>
+  <div class="post d-flex" @click="inPost">
+    <div class="d-flex align-center justify-center" style="width: 15%">
+      <v-icon icon="mdi-forum" size="40"></v-icon>
     </div>
+    <div class="d-flex" style="width: 60%">
+      <v-card color="transparent" border="0" variant="tonal">
+        <div class="v-card__text">
+          <v-card-title>
+            {{ post.title }}
+          </v-card-title>
+          <v-card-subtitle>
+            {{ post.created_at.substring(0, 10) }}
+          </v-card-subtitle>
+          <v-card-text>
+            {{ post.content }}
+          </v-card-text>
+          <a v-for="i in post.tags">
+            {{ i.name }}
+          </a>
+        </div>
+      </v-card>
+    </div>
+    <div class="d-flex align-center justify-end" style="width: 25%">
+      <div
+        class="d-flex flex-column align-center justify-center"
+        style="width: 60%"
+      >
+        <a>{{ post.views }}</a>
+        <a>Views</a>
+      </div>
+    </div>
+  </div>
 </template>
 <script setup>
 import router from "@/router/router";
 
 const props = defineProps({
-        id: {
-            type: Number,
-            required: true
-        },
-        title: {
-            type: String,
-            required: true
-        },
-        update_at: {
-            type: String,
-            required: true
-        },
-        content: {
-            type: String,
-            required: true
-        },
-        views: {
-            type: Number,
-            required: true
-        },
-        tag: Object
-    })
-;
+  post: {
+    type: Object,
+    required: true,
+  },
+});
 
 function inPost() {
-    router.push("/posts/" + props.id);
+  router.push("/posts/" + props.post.id);
 }
 </script>
 <style scoped>
 .post {
-    background-color: rgba(232, 230, 230, 1);
-    height: 150px;
-    width: 650px;
-    margin: 20px;
-    border-radius: 10px;
-    transition: 0.5s;
+  background-color: rgba(232, 230, 230, 1);
+  height: 150px;
+  width: 650px;
+  margin: 20px;
+  border-radius: 10px;
+  transition: 0.5s;
 }
 
 .post:hover {
-    background-color: rgba(173, 173, 173, 1);
-    transition: 100ms;
+  background-color: rgba(173, 173, 173, 1);
+  transition: 100ms;
 }
 
 .v-card__text {
-    color: black;
+  color: black;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,81 +1,82 @@
 <template>
-    <div class="d-flex justify-center" style="width: 100%;">
-        <div class="d-flex justify-start flex-column" style=" width: 100%;height: 100%">
-            <div class="d-flex" style="height: 100px">
-
-            </div>
-        </div>
-        <div class="d-flex flex-column align-center justify-center">
-            <Search/>
-            <div class="scroll-container light">
-                <Post v-for="post in posts.data"
-                      :id="post.id"
-                      :title="post.title"
-                      :update_at="post.created_at"
-                      :views="post.views"
-                      :content="post.content"
-                      :tag="post.tags"
-                />
-            </div>
-        </div>
-        <div class="d-flex justify-center align-end" style="width: 100%">
-            <div class="d-flex align-center" style="height: 20%">
-                <v-btn class="add-post-btn" icon="mdi-plus" size="large" color="#D9D9D9" @click="addPost"></v-btn>
-            </div>
-        </div>
+  <div class="d-flex justify-center" style="width: 100%">
+    <div
+      class="d-flex justify-start flex-column"
+      style="width: 100%; height: 100%"
+    >
+      <div class="d-flex" style="height: 100px"></div>
     </div>
+    <div class="d-flex flex-column align-center justify-center">
+      <Search />
+      <div class="scroll-container light">
+        <Post v-for="post in posts.data" :post="post" />
+      </div>
+    </div>
+    <div class="d-flex justify-center align-end" style="width: 100%">
+      <div class="d-flex align-center" style="height: 20%">
+        <v-btn
+          class="add-post-btn"
+          icon="mdi-plus"
+          size="large"
+          color="#D9D9D9"
+          @click="addPost"
+        ></v-btn>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import Post from "@/components/Home/Post.vue";
 import Search from "@/components/Home/Search.vue";
 
-import axios from 'axios';
-import {useRouter} from 'vue-router';
-import {onMounted, reactive} from "vue";
+import axios from "axios";
+import { useRouter } from "vue-router";
+import { onMounted, reactive } from "vue";
 
 const router = useRouter();
 
 const posts = reactive({
-    data: null,
-    error: null
-})
+  data: null,
+  error: null,
+});
 
 async function addPost() {
-    await router.push('/posts/add');
+  await router.push("/posts/add");
 }
 
-async function fetchPosts(){
-    try {
-        const response = await axios.get(import.meta.env.VITE_APP_API_URL + "/posts")
-        posts.data = response.data
-    } catch (error) {
-        posts.error = error;
-        await router.push({path: import.meta.env.VITE_APP_ERROR_ROUTER});
-    }
+async function fetchPosts() {
+  try {
+    const response = await axios.get(
+      import.meta.env.VITE_APP_API_URL + "/posts"
+    );
+    posts.data = response.data;
+  } catch (error) {
+    posts.error = error;
+    await router.push({ path: import.meta.env.VITE_APP_ERROR_ROUTER });
+  }
 }
 
 onMounted(fetchPosts);
-
 </script>
 
 <style scoped>
 .scroll-container {
-    overflow-y: scroll;
-    height: 80vh;
+  overflow-y: scroll;
+  height: 80vh;
 }
 
 .light::-webkit-scrollbar {
-    width: 15px;
+  width: 15px;
 }
 
 .light::-webkit-scrollbar-thumb {
-    background: #b0b0b0;
-    border: solid 3px #e6e6e6;
-    border-radius: 20px;
+  background: #b0b0b0;
+  border: solid 3px #e6e6e6;
+  border-radius: 20px;
 }
 
 .light::-webkit-scrollbar-thumb:hover {
-    background: black;
+  background: black;
 }
 </style>
