5d97d5bada0c1ca363a15c6803c6971e26f81cc1
ian901204 <46097196+ian901204@users.noreply.github.com>
2023-04-24 22:34:12 +0800

    feat: add admin router and view page (#14)

--- src/main.js
+++ src/main.js
@@ -3,9 +3,20 @@ import App from './App.vue';
 import router from './router/router.js';
 import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
+import { VDataTable } from 'vuetify/labs/VDataTable';
 import '@/assets/main.css';
 
-const vuetify = createVuetify();
+const vuetify = createVuetify({
+    components: {
+        VDataTable,
+    },
+    defaults: {
+        VDataTable: {
+            fixedHeader: true,
+            noDataText: 'Results not found',
+        },
+    },
+});
 
 const app = createApp(App);
 
---
+++ src/views/AdminTagsView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="d-flex justify-center" style="width: 100%">
+        <div class="d-flex flex-column" style="width: 80%; padding-top: 60px">
+            <div class="d-flex justify-space-between align-center" style="padding-left: 10px">
+                <Search />
+                <div class="d-flex justify-space-between">
+                    <v-btn>勾選刪除</v-btn>
+                </div>
+            </div>
+            <TagsDataTable />
+        </div>
+    </div>
+</template>
+<script setup>
+import Search from '@/components/Home/Search.vue';
+import TagsDataTable from '@/components/Admin/TagsDataTable.vue';
+</script>
