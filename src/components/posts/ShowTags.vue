25e15e105df408e9d2dfacae91d2fa1ef9f96145
ian901204 <scckate444@gmail.com>
2023-04-24 17:44:25 +0800

    refactor showtags function remove

--- src/components/posts/ShowTags.vue
+++ src/components/posts/ShowTags.vue
@@ -1,19 +1,13 @@
 <template>
-    <div class="d-flex">
-        <a v-if="checkNull" v-for="i in tagsData">#{{ i.names }}</a>
-    </div>
+  <div class="d-flex">
+    <a v-if="tagsData" v-for="i in tagsData">#{{ i.names }}</a>
+  </div>
 </template>
 <script setup>
-import {computed} from "vue";
-
 const props = defineProps({
-    tagsData: {
-        type: Object,
-        required: true
-    }
-})
-
-const checkNull = computed(() => {
-    return props.tagsData == null;
-})
+  tagsData: {
+    type: Object,
+    required: true,
+  },
+});
 </script>
