3c3f10aaaa15638713377826f178501aec7f121d
ian901204 <scckate444@gmail.com>
2023-05-01 21:47:27 +0800

    feat: add pinia and persistedstate into main.js

--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ import router from './router/router.js';
 import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
 import { VDataTable } from 'vuetify/labs/VDataTable';
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import '@/assets/main.css';
-
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
 const vuetify = createVuetify({
     components: {
         VDataTable,
@@ -22,4 +25,5 @@ const app = createApp(App);
 
 app.use(router);
 app.use(vuetify);
+app.use(pinia);
 app.mount('#app');
