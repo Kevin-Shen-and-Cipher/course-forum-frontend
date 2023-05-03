64aed34dc7e30bc8a0aeed8c0773ade0b65b301c
ian901204 <scckate444@gmail.com>
2023-05-03 20:27:01 +0800

    feat: add alert to all page

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,10 @@
     <v-app>
         <div class="d-flex flex-column" style="height: 100%">
             <div class="d-flex flex-column" style="height: 64px">
-                <top-bar/>
+                <top-bar />
             </div>
             <div class="d-flex main-section">
+                <PopOut />
                 <router-view />
             </div>
             <div class="d-flex" style="height: 64px">
@@ -17,6 +18,7 @@
 import { RouterView } from 'vue-router';
 import Footer from '@/components/Footer.vue';
 import TopBar from '@/components/TopBar.vue';
+import PopOut from '@/components/PopOut.vue';
 import { onMounted } from 'vue';
 onMounted(() => {
     document.title = import.meta.env.VITE_APP_TITLE;
