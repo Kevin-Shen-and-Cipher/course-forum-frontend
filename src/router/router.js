53734ca7d772569b74a2d2d2253a93b7e5ee2e5f
ian901204 <scckate444@gmail.com>
2023-05-04 09:24:49 +0800

    feat: first remove data

--- src/router/router.js
+++ src/router/router.js
@@ -1,15 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import {useAuthStore} from "@/store/auth";
-const alertStore = useAuthStore();
-const ifAuthenticated = (to, from, next) => {
-    if (alertStore.token !== null) {
-      next();
-      return;
-    }
-    router.push({ 
-      name: 'login'
-    });
-   };
 const routes = [
     {
         path: '/',
@@ -35,25 +24,21 @@ const routes = [
         path: '/posts/add',
         name: 'AddPosts',
         component: () => import('@/views/AddPostView.vue'),
-        beforeEnter: ifAuthenticated,
     },
     {
         path: '/admin/tags',
         name: 'AdminTags',
         component: () => import('@/views/AdminTagsView.vue'),
-        beforeEnter: ifAuthenticated,
     },
     {
         path: '/admin/posts',
         name: 'AdminPosts',
         component: () => import('@/views/AdminPostsView.vue'),
-        beforeEnter: ifAuthenticated,
     },
     {
         path: '/admin/posts/check/:id',
         name: 'AdminPostsCheck',
         component: () => import('@/views/CheckPosts.vue'),
-        beforeEnter: ifAuthenticated,
     },
 ];
 
